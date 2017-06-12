424aa60b8541233fe580135251b8d1e1dcca2bfc
victorkipkoech <victor.kipkoech94@gmail.com>
2017-06-11 19:57:20 -0700

    case python code

---
+++ case.py
@@ -0,0 +1,9 @@
+message = 'Hello world'
+print(message.lower())
+print(message.upper())
+print(message.swapcase())
+name=input('What is your name?')
+name=name.lower()
+country =input('What country do u come from?')
+country=country.upper()
+print ('Hello, ' + name +',You\'re from '+ country )
