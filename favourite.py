541b18e539f905353df7f9cf07973fdac98758fa
victorkipkoech <victor.kipkoech94@gmail.com>
2017-06-11 19:57:07 -0700

    favourite

---
+++ favourite.py
@@ -0,0 +1,5 @@
+favouriteTeam=input('Which is your favourite team? ')
+if favouriteTeam.lower()=='chelsea':
+    print("Go chelsea Go!! ")
+    print("Its good if you also support barcelona!")
+print("watching football is much fun")
