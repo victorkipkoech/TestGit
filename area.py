630a2d038e25ad13b8f3a082f629ef1030e8bbd9
victorkipkoech <victor.kipkoech94@gmail.com>
2017-06-11 19:48:29 -0700

    third commit

---
+++ area.py
@@ -0,0 +1,8 @@
+area =0
+height=input('Enter value one:')
+length =input('Enter value two:')
+#area = height * length /2
+perimeter=int(height) + int(length)
+print(perimeter)
+#print ('The area of the triangle is %f ' % area)
+print('My value is %3d ' %15565.533556)
