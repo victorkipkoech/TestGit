ffff8d306a8fa45804259c6c75a89ad20750caca
victorkipkoech <victor.kipkoech94@gmail.com>
2017-06-11 20:02:49 -0700

    getting a free toaster

---
+++ freeToaster.py
@@ -0,0 +1,8 @@
+freeToaster =False
+deposit =int(input("How much do you  wanna deposit? "))
+if deposit >100:
+    freeToaster =True
+
+if freeToaster:
+    print("Free toaster for you!")
+print("Have a nyc day")
